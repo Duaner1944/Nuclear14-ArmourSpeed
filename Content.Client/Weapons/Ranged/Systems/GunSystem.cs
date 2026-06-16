3957e3f39ca1d648f22048f1d688407a75182929
Vixting <vixting@gmail.com>
2026-06-16 23:09:24 +0100

    fix

--- Content.Client/Weapons/Ranged/Systems/GunSystem.cs
+++ Content.Client/Weapons/Ranged/Systems/GunSystem.cs
@@ -370,10 +370,7 @@ public sealed partial class GunSystem : SharedGunSystem
                 case AmmoComponent newAmmo:
                     CreateAndFireProjectiles(ent!.Value, newAmmo);
                     Recoil(user, mapDirection, gun.CameraRecoilScalarModified);
-                    if (IsClientSide(ent!.Value))
-                        Del(ent.Value);
-                    else
-                        RemoveShootable(ent.Value);
+                    RemoveShootable(ent.Value);
                     break;
                 case HitscanPrototype:
                     PredictHitscan(gunUid, fromCoordinates, mapDirection, mapDirection.ToAngle(), gun.Target, user);
